4d9e18b4c4712fe0732110bdb6b9b8d789183bd3
Satyam Seth <satyam1998.1998@gmail.com>
2024-06-21 02:55:41 +0530

    add recod outbound phone calls example

---
+++ Official Doc/Programmable Voice/2.tutorials/2.1.record phone calls/outbound/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export TO_NUMBER="+91XXXXXXXXXX"
+export FROM_NUMBER="+1XXXXXXXXXX"
+
+export TWILIO_ACCOUNT_SID="-----------------------------"
+export TWILIO_AUTH_TOKEN="-----------------------------"
+
+python app.py
